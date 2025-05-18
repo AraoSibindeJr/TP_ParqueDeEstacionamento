d18fe645f7e28e61f632eb0dc937c190a99dcc5d
Martin Langa <167089903+elverolanga@users.noreply.github.com>
2025-05-18 20:27:01 +0200

    featix: sdk config

--- parking_lot/feramentas/VeiculoRepositorio.java
+++ parking_lot/feramentas/VeiculoRepositorio.java
@@ -32,6 +32,6 @@ public class VeiculoRepositorio implements IRepositorio{
     }
     
     public Veiculo fileToObject(){
-        StringTokenizer
+        StringTokenizer st = new StringTokenizer();
     }
 }
